92d4f62397b6afeeb7d06a314c323fd0584df7a6
Konami33 <yasinarafat9889@gmail.com>
2024-10-25 05:06:03 +0600

    api-gateway

--- api-gateway-service/src/routes/proxyRoutes.js
+++ api-gateway-service/src/routes/proxyRoutes.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 const proxyController = require('../controllers/proxyController');
 
-// Define routes for different services
 router.use('/user-service', proxyController.proxyToService1);
 router.use('/payment-service', proxyController.proxyToService3);
 // router.use('/train-service', proxyController.proxyToService2);
