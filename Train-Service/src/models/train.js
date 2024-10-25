77b5ca5a99feebdd13f07b909bfa8c9cf1ee0257
Konami33 <yasinarafat9889@gmail.com>
2024-10-25 06:50:14 +0600

    booking system

--- Train-Service/src/models/train.js
+++ Train-Service/src/models/train.js
@@ -1,33 +1,7 @@
 const sequelize = require('../config/database'); // Adjust path as necessary
 const { DataTypes } = require('sequelize');
 
-<<<<<<< HEAD
-class Train extends Model {
-  // Method to calculate free seats
-  getFreeSeatsCount() {
-    let freeCount = 0;
-    this.seatStatus.forEach(coach => {
-      coach.forEach(seat => {
-        if (seat === 'FREE') {
-          freeCount++;
-        }
-      });
-    });
-    return freeCount;
-  }
-}
-
-// Define the Train model
-Train.init({
-  id: {clear
-    
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true,
-  },
-=======
 const Train = sequelize.define('Train', {
->>>>>>> 466858256b934e42ea7155fedb8440db692482b0
   name: {
     type: DataTypes.STRING,
     allowNull: false,
@@ -57,16 +31,7 @@ const Train = sequelize.define('Train', {
     allowNull: false,
   },
   seatStatus: {
-<<<<<<< HEAD
-    type: DataTypes.JSONB,
-    allowNull: false,
-    defaultValue: Array(10).fill(Array(10).fill('FREE')), // Example static value for testing
-  },
-  frequency: {
-    type: DataTypes.ARRAY(DataTypes.STRING),
-=======
     type: DataTypes.ARRAY(DataTypes.STRING), // Ensure this is set correctly
->>>>>>> 466858256b934e42ea7155fedb8440db692482b0
     allowNull: false,
   },
   
